c3f0fcc6dec338b5005b2eddefb62deaf1ced18d
Coleen Ursal <zaian2929@gmail.com>
2017-08-01 10:47:52 +0800

    first commit

---
+++ myphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Logical Operators</title>
+</head>
+<body>
+<?php
+$d = date("Fri");
+if($d == "Fri"){
+    echo "Have a nice weekend!";
+} elseif($d == "Sun"){
+    echo "Have a nice Sunday!";
+} else{
+    echo "Have a nice day!";
+}
+?>
+
+</body>
+</html>
