be4bf5879ae6c43714e0e6dd88eecba1f7cf3ade
Coleen Ursal <zaian2929@gmail.com>
2017-08-03 18:48:11 +0800

    test commit

--- myphp.php
+++ myphp.php
@@ -8,9 +8,14 @@
 	$load = array(3,4,"Sherwin",6,56);
 	echo $load[2]."</br>";
 
-	$mixed = array(5,"what","oh\,no!",array(34,"arvin",6,56));
+	$mixed = array(5,"what","oh,no!",array(34,"arvin",6,56));
 	echo $mixed[3][1];
 ?>
-
+<pre>
+<?php
+	print_r($load);
+	print_r($mixed);
+?>
+</pre>
 </body>
 </html>
